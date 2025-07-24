f82e6af50ce4d14ba9ae5f959ac17861b4bf892b
Yuumi0221 <Yuumi0221@users.noreply.github.com>
2025-07-24 14:33:40 +0000

    deploy: 351ab2d15779b747a5571af9cecce0bc940c67fb

---
+++ assets/post.DAIeTBbP.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.CHqXhTU0.js";import{p as t}from"./app.Bud39-gm.js";import{U as e}from"./framework.CVaIbib-.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
